79b431b517ad5d7715880c065e4bd6621c5b5e25
Cristina Dinescu <crina91@gmail.com>
2020-02-28 22:01:46 +0200

    Used @EnableDiscoveryClient in  order to have configurable code for other discovery services (e.g. Consul).

--- src/main/java/com/vitanum/foodservice/FoodServiceApplication.java
+++ src/main/java/com/vitanum/foodservice/FoodServiceApplication.java
@@ -19,12 +19,12 @@ import com.vitanum.foodservice.controller.FoodServiceImpl;
 import com.vitanum.foodservice.utils.UriComponentBuilderUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class FoodServiceApplication {
 
     public static void main(String[] args) {
